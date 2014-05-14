33127355c829ce9a7507a4fb3863d436cf8959b9
maximromanov <romanov.maxim@gmail.com>
2014-05-14 11:17:48 -0400

    one more update

--- MCIW_JsonExercise/palette.js
+++ MCIW_JsonExercise/palette.js
@@ -1,9 +1,9 @@
 function palette() {
-	var seeds = Array('00','CC','33','66','99','FF');
+	var seeds = Array('00','CC','33','66','99','FF', 'F0', 'F8', 'C8', 'C0');
 	var palette = [];
-	for ( i=0; i<6; i++ ) {
-		for ( j=0; j<6; j++ ) {
-			for( k=0; k<6; k++ ) {
+	for ( i=0; i<10; i++ ) {
+		for ( j=0; j<10; j++ ) {
+			for( k=0; k<10; k++ ) {
 				palette.push( '#' + seeds[i] + seeds[j] + seeds[k] );
 			}
 		}
